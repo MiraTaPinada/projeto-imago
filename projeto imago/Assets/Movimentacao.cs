0fb5c566be0c587880c6ad2cb18cb6b64b67c025
MiraTaPinada <86327387+MiraTaPinada@users.noreply.github.com>
2022-02-15 20:12:21 -0300

    add moviment

---
+++ projeto imago/Assets/Movimentacao.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace projeto_imago
+{
+    public class movimentacao : MonoBehaviour
+    {
+        public float pulo = 6f;
+        public float speed = 6f;
+        private Rigidbody2D rb;
+        private bool canJump = true;
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        void FixedUpdate()
+        {
+            MoveWithKeys();
+        }
+
+        void MoveWithKeys()
+        {
+            var left = Input.GetKey(KeyCode.A);
+            var right = Input.GetKey(KeyCode.D);
+
+            var horizontal = left ? -1 : right ? 1 : 0;
+
+            transform.position += new Vector3(horizontal, 0, 0).normalized * speed * Time.deltaTime;
+
+
+            var jump = Input.GetKey(KeyCode.Space);
+            if (jump&&canJump)
+            {
+                rb.AddForce(Vector2.up *pulo , ForceMode2D.Impulse);
+                canJump = false;
+            }
+
+        }
+        void OnCollisionEnter(Collision other)
+        {
+            Debug.Log("shgdf");
+            canJump = true;
+        }
+    }
+}
